187eceac81f98a5209d342e160bba1d8eb44119a
Ninh Tran <manhha30102001@gmail.com>
2024-03-17 23:47:54 +0700

    detail staff

--- src/components/molecules/staff/popup_detail/guide.tsx
+++ src/components/molecules/staff/popup_detail/guide.tsx
@@ -1,25 +1,12 @@
-import { FC, useState, useRef } from "react";
-import Box from '@mui/material/Box';
-import { DataGrid, GridColDef, GridRowId } from "@mui/x-data-grid";
+import { FC, useState } from "react";
+import { GridRowId } from "@mui/x-data-grid";
+import { BaseGrid } from "components/molecules/base_grid";
 
 interface Props{}
 export const Guide: FC<Props> = () => {
-
-    const [isAddingRow, setIsAddingRow] = useState(false);
-    const gridRef = useRef<any>(null);
     const [selectedRows, setSelectedRows] = useState<GridRowId[]>([]);
 
-    const columns: GridColDef[] = [
-        { field: 'id', headerName: 'STT', width: 90 },
-        { field: 'student_name', headerName: 'Tên NCS', width: 150 },
-        { field: 'training', headerName: 'Khóa đào tạo', width: 150 },
-        { field: 'num_decision', headerName: 'Số QĐ Giao Luận án, Luận văn, Đồ án', width: 150 },
-        { field: 'num_instructors', headerName: 'Số QĐ Giao Luận án, Luận văn, Đồ án', width: 150 },
-        { field: 'main_instructors', headerName: 'HD chính', width: 150 },
-        { field: 'num_lesion', headerName: 'Số tiết quy đổi', width: 150 },
-    ];
-
-    const [dataSource, setDataSource] = useState([
+    const [rows, setRows] = useState([
       { id: 1, student_name: 'Vũ Trung Kiên', training: 'CT4', num_decision:'1677 / QĐ-HVM', num_instructors:'2', main_instructors:'', num_lesion:'15'},
       { id: 2, student_name: 'Phạm Thị Kim', training: 'CT4', num_decision:'1677 / QĐ-HVM', num_instructors:'2', main_instructors:'', num_lesion:'15'},
       { id: 3, student_name: 'Phan Kim Liên', training: 'CT4', num_decision:'1677 / QĐ-HVM', num_instructors:'2', main_instructors:'', num_lesion:'15'},
@@ -27,56 +14,50 @@ export const Guide: FC<Props> = () => {
     ]);
   
     const handleAddRow = () => {
-        setIsAddingRow(true);     
-
         const newRow = {
-        id: dataSource.length + 1,
-        position_id: '',
-        position_name: '',
+          id: rows.length + 1,
+          student_name: '',
+          training: '',
+          num_decision: '',
+          num_instructors: '',
+          main_instructors: '',
+          num_lesion: '',
+        };
+        setRows([...rows, newRow]);
       };
-      setDataSource([...dataSource, newRow]);
-    };
-  
-    const handleSave = () => {
-  
-    };
-  
-    const handleRowSelectionChange = (selection: GridRowId[]) => {
-      setSelectedRows(selection);
-    };
-  
-    const handleDelete = () => {
-      const updatedDataSource = dataSource.filter((row: any) => !selectedRows.includes(row.id));
-      setDataSource(updatedDataSource);
-      setSelectedRows([]);
-    };
-  
+    
+      const handleSave = () => {
+        // Handle save logic here
+      };
+    
+      const handleDelete = () => {
+        const updatedRows = rows.filter((row) => !selectedRows.includes(row.id));
+        setRows(updatedRows);
+        setSelectedRows([]);
+      };
+    
+      const handleRowSelectionChange = (selection: GridRowId[]) => {
+        setSelectedRows(selection);
+      };
   
     return (
-      <div>
-        <button onClick={handleSave}>Save</button>
-        <button onClick={handleDelete}>Delete</button>
-        <button onClick={handleAddRow}>Add Row</button>
-  
-        <Box sx={{ height: 400, width: '100%' }}>
-          <DataGrid
-            rows={dataSource}
-            columns={columns}
-            ref={gridRef}
-            initialState={{
-              pagination: {
-                paginationModel: {
-                  pageSize: 5,
-                },
-              },
-            }}
-            pageSizeOptions={[5]}
-            checkboxSelection
-            disableRowSelectionOnClick
-            onRowSelectionModelChange={handleRowSelectionChange}
-            rowSelectionModel={selectedRows}
-          />
-        </Box>
-      </div>
+        <BaseGrid
+        columns={[
+            { field: 'id', headerName: 'STT', width: 90 },
+            { field: 'student_name', headerName: 'Tên NCS', width: 150, editable: true },
+            { field: 'training', headerName: 'Khóa đào tạo', width: 150, editable: true },
+            { field: 'num_decision', headerName: 'Số QĐ Giao Luận án, Luận văn, Đồ án', width: 300, editable: true },
+            { field: 'num_instructors', headerName: 'Số người HD', width: 150, editable: true },
+            { field: 'main_instructors', headerName: 'HD chính', width: 150, editable: true },
+            { field: 'num_lesion', headerName: 'Số tiết quy đổi', width: 150, editable: true },
+        ]}
+        rows={rows}
+        title="Example Grid"
+        onSave={handleSave}
+        onDelete={handleDelete}
+        onAddRow={handleAddRow}
+        onRowSelectionChange={handleRowSelectionChange}
+        selectedRows={selectedRows}
+        />
     );
 };
