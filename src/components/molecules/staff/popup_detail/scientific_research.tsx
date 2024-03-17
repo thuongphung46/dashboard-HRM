41c8085ab4df8a5e0cc37b7dd333360cb670a1e7
Ninh Tran <manhha30102001@gmail.com>
2024-03-18 00:50:51 +0700

    tab NCKH

--- src/components/molecules/staff/popup_detail/scientific_research.tsx
+++ src/components/molecules/staff/popup_detail/scientific_research.tsx
@@ -8,28 +8,52 @@ export const ScientificResearch: FC<Props> = () => {
     const [selectedRows, setSelectedRows] = useState<GridRowId[]>([]);
 
     const [rows, setRows] = useState([
-      { id: 1, student_name: 'Vũ Trung Kiên', training: 'CT4', num_decision:'1677 / QĐ-HVM', num_instructors:'2', main_instructors:'', num_lesion:'15'},
-    //   { id: 2, student_name: 'Phạm Thị Kim', training: 'CT4', num_decision:'1677 / QĐ-HVM', num_instructors:'2', main_instructors:'', num_lesion:'15'},
-    //   { id: 3, student_name: 'Phan Kim Liên', training: 'CT4', num_decision:'1677 / QĐ-HVM', num_instructors:'2', main_instructors:'', num_lesion:'15'},
-    //   { id: 4, student_name: 'Trần Minh Đức', training: 'CT4', num_decision:'1677 / QĐ-HVM', num_instructors:'2', main_instructors:'', num_lesion:'15'},
+        { 
+            id: 1, 
+            certificate_name: 'Nghiên cứu, tích hợp trợ giảng ảo vào hệ thống đào tạo kết hợp của HVKTMM', 
+            num_validation: 'Chủ trì', 
+            date_validation:'Cơ sở (Học viện)', 
+            num_people:'2', 
+            role:'', 
+            hours:'',
+        },
+        { 
+            id: 1, 
+            topic_name: 'Nghiên cứu, tích hợp trợ giảng ảo vào hệ thống đào tạo kết hợp của HVKTMM', 
+            position: 'Chủ trì', 
+            topic_level:'Cơ sở (Học viện)', 
+            accept_date:'2', 
+            result:'', 
+            hours:'',
+        },
+        
     ]);
   
-    const handleAddRow = () => {
+    const handleAddRow1 = () => {
         const newRow = {
           id: rows.length + 1,
-          student_name: '',
-          training: '',
-          num_decision: '',
-          num_instructors: '',
-          main_instructors: '',
-          num_lesion: '',
+          certificate_name: '',
+          num_validation: '',
+          date_validation: '',
+          num_people: '',
+          role: '',
+          hours: '',
         };
         setRows([...rows, newRow]);
       };
 
-    const handleAddRow2 = (data:any) =>{
-
-    }
+      const handleAddRow2 = () => {
+        const newRow = {
+          id: rows.length + 1,
+          topic_name: '',
+          position: '',
+          topic_level: '',
+          accept_date: '',
+          result: '',
+          hours:''
+        };
+        setRows([...rows, newRow]);
+      };
     
       const handleSave = () => {
         // Handle save logic here
@@ -62,7 +86,7 @@ export const ScientificResearch: FC<Props> = () => {
                 title="C.1 Đề tài, dự án (Phụ lục II.1 Quyết định số 1409/QĐ-HVM)"
                 onSave={handleSave}
                 onDelete={handleDelete}
-                onAddRow={handleAddRow}
+                onAddRow={handleAddRow1}
                 onRowSelectionChange={handleRowSelectionChange}
                 selectedRows={selectedRows}
                 />
@@ -71,18 +95,18 @@ export const ScientificResearch: FC<Props> = () => {
                 <BaseGrid
                 columns={[
                     { field: 'id', headerName: 'STT', width: 90 },
-                    { field: 'student_name', headerName: 'Tên NCS', width: 150, editable: true },
-                    { field: 'training', headerName: 'Khóa đào tạo', width: 150, editable: true },
-                    { field: 'num_decision', headerName: 'Số QĐ Giao Luận án, Luận văn, Đồ án', width: 300, editable: true },
-                    { field: 'num_instructors', headerName: 'Số người HD', width: 150, editable: true },
-                    { field: 'main_instructors', headerName: 'HD chính', width: 150, editable: true },
-                    { field: 'num_lesion', headerName: 'Số tiết quy đổi', width: 150, editable: true },
+                    { field: 'certificate_name', headerName: 'Tên bằng sáng chế, giải thưởng', width: 150, editable: true },
+                    { field: 'num_validation', headerName: 'Số QĐ công nhận', width: 150, editable: true },
+                    { field: 'date_validation', headerName: 'Ngày QĐ công nhận', width: 300, editable: true },
+                    { field: 'num_people', headerName: 'Số người', width: 150, editable: true },
+                    { field: 'role', headerName: 'Tác giả chính/ Thành viên', width: 150, editable: true },
+                    { field: 'hours', headerName: 'Số giờ quy đổi', width: 150, editable: true },
                 ]}
                 rows={rows}
                 title="C.2 Bài báo khoa học(Phụ lục II.3 Quyết định số 1409/QĐ-HVM)"
                 onSave={handleSave}
                 onDelete={handleDelete}
-                onAddRow={handleAddRow}
+                onAddRow={handleAddRow2}
                 onRowSelectionChange={handleRowSelectionChange}
                 selectedRows={selectedRows}
                 />
