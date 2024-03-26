374c8b6df3aca5361074f82cb7ead2adfe94d321
DuyManhKMA <manhha30102001@gmail.com>
2024-03-27 00:53:04 +0700

    feat: filter staff

--- src/components/molecules/staff/list_star/index.tsx
+++ src/components/molecules/staff/list_star/index.tsx
@@ -1,12 +1,13 @@
+import { Input, MenuItem, Select } from "@mui/material";
 import Box from "@mui/material/Box";
 import { BaseGrid } from "components/atoms/datagrid";
-import { FC, useCallback, useEffect, useState } from "react";
-import { columns } from "./columns";
+import { FC, useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   GetListStaffParams,
   useGetListStaff,
 } from "services/hooks/useGetListStaff";
+import { columns } from "./columns";
 
 interface Props {}
 interface StaffData {
@@ -22,7 +23,7 @@ export const ListStaff: FC<Props> = () => {
   const navigate = useNavigate();
   const [params, setParams] = useState<GetListStaffParams>({
     query: "",
-    active: 1,
+    active: undefined,
     page: 0,
     size: 25,
   });
@@ -38,6 +39,24 @@ export const ListStaff: FC<Props> = () => {
   return (
     <div>
       <Box>
+        <Input
+          value={params.query}
+          onChange={(e) => setParams({ ...params, query: e.target.value })}
+          placeholder="Tìm kiếm nhân viên"
+        />
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={params.active}
+          label="Age"
+          onChange={(e) => {
+            setParams({ ...params, active: e.target.value as 0 | 1 });
+          }}
+        >
+          <MenuItem value={undefined}>Tất cả</MenuItem>
+          <MenuItem value={1}>Đang làm việc</MenuItem>
+          <MenuItem value={0}>Đã nghỉ việc</MenuItem>
+        </Select>
         {loading ? (
           <div>Loading...</div>
         ) : (
