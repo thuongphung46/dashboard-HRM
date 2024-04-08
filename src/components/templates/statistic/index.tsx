8c778c1784cbef7570c90bf821da61563a5087aa
Tran Thi Hoai Ninh <ninhtth1207@gmail.com>
2024-04-08 15:49:14 +0700

    get list department

--- src/components/templates/statistic/index.tsx
+++ src/components/templates/statistic/index.tsx
@@ -1,8 +1,28 @@
-import { Margin } from "@mui/icons-material";
+import React, { useState, useEffect, FC } from 'react';
 import { BasicColor } from "components/molecules/statistic/chart";
 import { GridStatistic } from "components/molecules/statistic/grid";
+import { useGetListDepartment } from 'services/hooks/useGetListDepartment';
 
-export const StatisticTemplate = () => {
+interface Props {}
+interface DepartmentData {
+  id: number;
+  name: string;
+  parentDeptId: string;
+  createdDate: string;
+  modifiedDate: string;
+  createdBy: string;
+  modifiedBy: string;
+}
+
+export const StatisticTemplate:FC<Props> = () => {
+  const [departmentList, setDepartmentList] = useState<DepartmentData[]>([]);
+  const { loading, data: departmentData } = useGetListDepartment();
+
+  useEffect(() => {
+    if (!loading && departmentData) {
+      setDepartmentList(departmentData);
+    }
+  }, []); // chỉ định mảng rỗng để chạy useEffect một lần khi trang được tải
 
   return (
     <div>
@@ -17,14 +37,14 @@ export const StatisticTemplate = () => {
         <input style={{margin:"5px"}} type="text" id="year" name="year"/>
 
         <label style={{margin:"5px"}} htmlFor="department">Khoa: </label>
-        <select style={{margin:"5px", width:"20px"}} id="department" name="department">
-          {/* {facultyList.map((faculty) => (
-            <option key={faculty.content.id} value={faculty.content.name}>
-              {faculty.content.name}
+        <select style={{margin:"5px"}} id="department">
+          {departmentList.map((department) => (
+            <option key={department.id} value={department.id}>
+              {department.name}
             </option>
-          ))} */}
+          ))}
         </select>
-      
+
 
         <label style={{margin:"5px"}} htmlFor="department">Bộ môn: </label>
         <input style={{margin:"5px"}} type="text" id="department" name="department" value="Bộ môn khoa học máy tính" />
--- src/services/hooks/useGetListDepartment.ts
+++ src/services/hooks/useGetListDepartment.ts
@@ -2,53 +2,24 @@ import { useEffect, useState } from "react";
 import { API_URL, NetWork } from "services/api";
 import { RESPONSE_CODE } from "services/api/config";
 
-// export type GetListStaffParams = {
-//   query?: string;
-//   active?: 0 | 1;
-//   page?: number;
-//   size?: number;
-// };
-
 export const useGetListDepartment = () => {
   const [data, setData] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
   const fetchData = async () => {
     setLoading(true);
-    const response = await NetWork.get(
-      API_URL.DEPARTMENT,
-      // deleteParamsNotUsing(params)
-    );
+    const response = await NetWork.get(`${API_URL.DEPARTMENT}`);
     if (response.status === RESPONSE_CODE.SUCCESS) {
-      setData(response?.data?.content?.data);
+      setData(response?.data?.content);
       setLoading(false);
     } else {
       setLoading(false);
     }
   };
+
   useEffect(() => {
     fetchData();
-  });
+  }, []);
+
   return { data, loading };
 };
-
-// export const useGetDepartment = (id: number) => {
-//   const [data, setData] = useState<any>({});
-//   const [loading, setLoading] = useState<boolean>(true);
-
-//   const fetchData = async () => {
-//     setLoading(true);
-//     const response = await NetWork.get(`${API_URL.STAFFS}/${id}`);
-//     if (response.status === RESPONSE_CODE.SUCCESS) {
-//       setData(response?.data?.content);
-//       setLoading(false);
-//     } else {
-//       setLoading(false);
-//     }
-//   };
-//   useEffect(() => {
-//     fetchData();
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [id]);
-//   return { data, loading };
-// };
