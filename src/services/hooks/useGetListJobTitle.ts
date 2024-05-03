76c925940920f3d4a810b9808f90bcfada5108ee
Tran Thi Hoai Ninh <ninhtth1207@gmail.com>
2024-05-03 16:38:31 +0700

    hooks JobTitle

--- src/services/hooks/useGetListJobTitle.ts
+++ src/services/hooks/useGetListJobTitle.ts
@@ -38,3 +38,44 @@ export const useGetListJobTitle = () => {
 
   return { data, loading };
 };
+
+export type createCreateJobTitleBody = {
+  code: string;
+  jobTitle: string;
+};
+
+const useCreateJobTitle = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const createJobTitle = async (body: createCreateJobTitleBody) => {
+    setLoading(true);
+    const response = await NetWork.post(`${API_URL.JOB_TITLE}`, body);
+    setLoading(false);
+    if (response.status === RESPONSE_CODE.SUCCESS) {
+      return response.data;
+    } else {
+      return null;
+    }
+  };
+
+  return { createJobTitle, loading };
+};
+
+export type updateJobTitleBody = {};
+
+const useUpdateJobTitle = (id: string | undefined) => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const updateJobTitle = async (id: string, body: updateJobTitleBody) => {
+    setLoading(true);
+    const response = await NetWork.patch(`${API_URL.JOB_TITLE}/${id}`, body);
+    setLoading(false);
+    if (response.status === RESPONSE_CODE.SUCCESS) {
+      return response.data;
+    } else {
+      return null;
+    }
+  };
+
+  return { updateJobTitle, loading };
+};
