2bf09827f4715468b0389c9f2ab61f341baf0edb
Tran Thi Hoai Ninh <ninhtth1207@gmail.com>
2024-05-04 09:47:00 +0700

    add reasion reduce

--- src/components/molecules/general/resion_columns/index.tsx
+++ src/components/molecules/general/resion_columns/index.tsx
@@ -2,20 +2,24 @@ import { FC, useState } from "react";
 import Box from '@mui/material/Box';
 import { GridColDef, GridRowId } from "@mui/x-data-grid";
 import { BaseGrid } from "components/atoms/datagrid";
-import { useGetListReasonReduce } from "services/hooks/useGetListReasonReduce";
+import { 
+  ReasonReduceType, 
+  useGetListReasonReduce, 
+  useReasonReduce 
+} from "services/hooks/useGetListReasonReduce";
+import { isNullOrEmpty } from "common/validation";
 
 interface Props{}
 export const GeneralResion: FC<Props> = () => {
-  
+  const [isAddingRow, setIsAddingRow] = useState(false);
   const [selectedRows, setSelectedRows] = useState<GridRowId[]>([]); 
-
   const columns: GridColDef[] = [
     { field: 'id', headerName: 'STT', width: 90 },
     {
       field: 'code',
       headerName: 'Mã lý do giảm trừ',
       width: 150,
-      editable: true,
+      editable: isAddingRow,
     },
     {
       field: 'name',
@@ -30,20 +34,38 @@ export const GeneralResion: FC<Props> = () => {
       editable: true,
     }
   ];
+  const { createReasonReduce, updateReasonReduce } = useReasonReduce();
+  const { data } = useGetListReasonReduce();
 
-  const { reasonReduce } = useGetListReasonReduce();
+  const handleSave = async (dataAdd: ReasonReduceType[]) => {
+    // getList id của các dòng mới thêm vào
+    const maxId = Math.max(...data.map((item) => item.id));
+    const newResionReduce = dataAdd.filter((item) => item.id > maxId);
+    if (!isNullOrEmpty(newResionReduce)) {
+      for (let i = 0; i < newResionReduce.length; i++) {
+        await createReasonReduce(newResionReduce[i]);
+      }
+    }
+    setSelectedRows([]);
+    setIsAddingRow(false);
+  };
+
+  const handleChange = (e: any) => {
+    // console.log("e", e);
+  };
+
   return (
     <div>
       <Box>
         <BaseGrid
           columns={columns}
-          rows={reasonReduce}
+          rows={data}
           title="Lí do giảm trừ"
-          onSave={() => {
-            /* Logic lưu cho lưới dữ liệu  */
-          }}
+          onSave={handleSave}
+          callBack={handleChange}
           onRowSelectionChange={setSelectedRows}
           selectedRows={selectedRows}
+          onPressAdd={() => setIsAddingRow(true)}
         />
       </Box>
     </div>
