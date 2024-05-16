45f808dd8970051122560db8808815d8400f7019
Phùng Hoài Thương <113285922+thuongphung46@users.noreply.github.com>
2024-05-16 18:11:48 +0700

    add devextreme

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import "devextreme/dist/css/dx.light.css";
+import themes from "devextreme/ui/themes";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,6 +17,13 @@ root.render(
   </Provider>
 );
 
+themes.initialized(() => {
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
