6f5ee51de9f01cf10c87619d7e706c890fdf108e
thuongphung <ct040249@actvn.edu.vn>
2024-05-24 19:19:30 +0700

    caculator over

--- src/components/molecules/staff/tab_analytic/overview/index.tsx
+++ src/components/molecules/staff/tab_analytic/overview/index.tsx
@@ -1,6 +1,5 @@
 import { Box, FormControl, MenuItem, Select } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid/DataGrid/DataGrid";
-import { GridColDef } from "@mui/x-data-grid/models/colDef/gridColDef";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import React, { useCallback, useEffect, useState } from "react";
 import { useGetListReasonReduce } from "services/hooks/useGetListReasonReduce";
 import { StaffDetail, StaffSummary } from "types/ApplicationType";
@@ -10,16 +9,19 @@ interface Props {
   all_data: StaffDetail;
   schoolYear: string;
 }
-export const Overview: React.FC<Props> = ({ data, all_data }) => {
+
+export const Overview: React.FC<Props> = ({ all_data, schoolYear }) => {
   const [sum, setSum] = useState({
-    Teaching: 0, // giảng dạy
-    examCourses: 0, //đánh giá học phần
-    instructProject: 0, //hợp đồng luận án,
-    numOfclassesTaught: 270, //số tiết phải giảng
-    numOfUnfinishedPeriods: 0, //số tiết chưa hoàn thành nghiên cứu KH
-    numOfPeriodsReduced: 0, //số tiết được giảm trừ
+    Teaching: 0,
+    examCourses: 0,
+    instructProject: 0,
+    numOfclassesTaught: 270,
+    numOfUnfinishedPeriods: 0,
+    numOfPeriodsReduced: 0,
   });
+
   const { data: ListReason } = useGetListReasonReduce();
+
   const handleChangeRearon = useCallback((data: any) => {
     setSum((prevSum) => ({
       ...prevSum,
@@ -28,43 +30,25 @@ export const Overview: React.FC<Props> = ({ data, all_data }) => {
   }, []);
 
   const columns: GridColDef[] = [
-    // { field: "schoolYear", headerName: "Năm học", width: 150 },
     { field: "contentWork", headerName: "Nội dung công việc", width: 400 },
     { field: "numberOfLesson", headerName: "Số tiết dạy", width: 150 },
     {
       field: "reasonReduce",
       headerName: "Lý do giảm trừ",
       width: 500,
-      renderCell: (prams) => {
-        if (prams.id === "IV") {
+      renderCell: (params) => {
+        if (params.id === "IV") {
           return (
-            <Box
-              sx={{
-                width: "500px",
-              }}
-            >
+            <Box sx={{ width: "500px" }}>
               <FormControl fullWidth>
-                <Select
-                  name="reason"
-                  label={"Lý do giảm trừ"}
-                  onChange={handleChangeRearon}
-                >
-                  {ListReason?.map((item, index) => {
-                    return (
-                      <MenuItem key={index} value={item.ratio}>
-                        <div
-                          style={{
-                            display: "flex",
-                            width: "100%",
-                            justifyContent: "space-between",
-                            alignItems: "center",
-                          }}
-                        >
-                          {item.name} <span>{item.ratio}%</span>
-                        </div>
-                      </MenuItem>
-                    );
-                  })}
+                <Select name="reason" label="Lý do giảm trừ" onChange={handleChangeRearon}>
+                  {ListReason?.map((item, index) => (
+                    <MenuItem key={index} value={item.ratio}>
+                      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                        {item.name} <span>{item.ratio}%</span>
+                      </div>
+                    </MenuItem>
+                  ))}
                 </Select>
               </FormControl>
             </Box>
@@ -75,44 +59,23 @@ export const Overview: React.FC<Props> = ({ data, all_data }) => {
   ];
 
   useEffect(() => {
-    let teachSum = 0;
-    let examCoursesSum = 0;
-    let instructProjectSum = 0;
-    let numOfUnfinishedPeriodsSum = 0;
-    all_data.teaching.forEach((item) => (teachSum += item.roundStandard));
-    all_data.examCourses.forEach(
-      (item) => (examCoursesSum += item.estimatedLesson)
-    );
-    all_data.instructProject.forEach(
-      (item) => (instructProjectSum += item.numberOfLesson)
-    );
-    all_data.project.forEach(
-      (item) => (numOfUnfinishedPeriodsSum += item.numberOfHours)
-    );
-    all_data.magazine.forEach(
-      (item) => (numOfUnfinishedPeriodsSum += item.numberOfHour)
-    );
-    all_data.invention.forEach(
-      (item) => (numOfUnfinishedPeriodsSum += item.numberOfHour)
-    );
-    all_data.book.forEach(
-      (item) => (numOfUnfinishedPeriodsSum += item.numberOfHour)
-    );
-    all_data.training.forEach(
-      (item) => (numOfUnfinishedPeriodsSum += item.numberOfHour)
-    );
-    all_data.buildingProgram.forEach(
-      (item) => (numOfUnfinishedPeriodsSum += item.numberOfHour)
-    );
+    const calculateSum = (key: keyof StaffDetail, condition: (item: any) => boolean) =>
+      all_data[key].reduce((acc: any, item: { roundStandard: any; estimatedLesson: any; numberOfLesson: any; numberOfHours: any; numberOfHour: any; }) => acc + (condition(item) ? item.roundStandard || item.estimatedLesson || item.numberOfLesson || item.numberOfHours || item.numberOfHour : 0), 0);
 
-    setSum((prevSum) => ({
+    const teachSum = calculateSum('teaching', item => item.schoolYear === schoolYear);
+    const examCoursesSum = calculateSum('examCourses', item => item.schoolYear === schoolYear);
+    const instructProjectSum = calculateSum('instructProject', item => item.schoolYear === schoolYear);
+    const numOfUnfinishedPeriodsSum = ['project', 'magazine', 'invention', 'book', 'training', 'buildingProgram']
+      .reduce((acc, key) => acc + calculateSum(key as keyof StaffDetail, item => item.schoolYear === schoolYear), 0);
+
+    setSum(prevSum => ({
       ...prevSum,
       Teaching: teachSum,
       examCourses: examCoursesSum,
       instructProject: instructProjectSum,
       numOfUnfinishedPeriods: numOfUnfinishedPeriodsSum,
     }));
-  }, [all_data]);
+  }, [all_data, schoolYear]);
 
   const contentStt = ["I", "II", "III", "IV", "V"];
   const contentWorkValues = [
@@ -122,51 +85,33 @@ export const Overview: React.FC<Props> = ({ data, all_data }) => {
     "Số tiết được giảm trừ",
     "Tổng số tiết vượt giờ đề nghị thanh toán (I - II - III + IV)",
   ];
-  //số tiết thực hiện
-  const numOfImplementationPeriods =
-    sum.Teaching + sum.examCourses + sum.instructProject;
 
-  //thay các giá trị tương tự vô mảng
+  const numOfImplementationPeriods = sum.Teaching + sum.examCourses + sum.instructProject;
+
   const numberOfLesson = [
     numOfImplementationPeriods,
     sum.numOfclassesTaught,
-    sum.numOfUnfinishedPeriods - 300 < 0
-      ? Math.abs(sum.numOfUnfinishedPeriods - 300)
-      : 0,
+    Math.max(sum.numOfUnfinishedPeriods - 300, 0),
     sum.numOfPeriodsReduced,
-    numOfImplementationPeriods -
-      sum.numOfclassesTaught -
-      sum.numOfUnfinishedPeriods +
-      sum.numOfPeriodsReduced,
+    numOfImplementationPeriods - sum.numOfclassesTaught - sum.numOfUnfinishedPeriods + sum.numOfPeriodsReduced,
   ];
 
-  const rows = [];
-  for (let i = 0; i < 5; i++) {
-    rows.push({
-      id: contentStt[i],
-      schoolYear: "",
-      contentWork: contentWorkValues[i],
-      numberOfLesson: numberOfLesson[i],
-      reasonReduce: "",
-    });
-  }
+  const rows = contentStt.map((id, index) => ({
+    id,
+    schoolYear: "",
+    contentWork: contentWorkValues[index],
+    numberOfLesson: numberOfLesson[index],
+    reasonReduce: "",
+  }));
 
   return (
-    <div>
-      <Box sx={{ height: 400, width: "100%" }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 5,
-              },
-            },
-          }}
-          pageSizeOptions={[5]}
-        />
-      </Box>
-    </div>
+    <Box sx={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{ pagination: { paginationModel: { pageSize: 5 } } }}
+        pageSizeOptions={[5]}
+      />
+    </Box>
   );
 };
