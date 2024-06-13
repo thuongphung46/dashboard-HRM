8c7de98320a67c575ea50064bb5c8fa4af94a803
Phùng Hoài Thương <113285922+thuongphung46@users.noreply.github.com>
2024-06-13 08:24:57 +0700

    css erro page

--- src/components/pages/error/index.tsx
+++ src/components/pages/error/index.tsx
@@ -1,7 +1,30 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./index.scss";
+
 export const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/model");
+  };
+
   return (
-    <div>
-      <h1>ErrorPage</h1>
+    <div className="error-page">
+      <h1 className="error-title">404 Not found</h1>
+      <p className="error-message">
+        Xin lỗi, trang bạn đang tìm kiếm không tồn tại.
+      </p>
+      <p
+        style={{
+          color: "#0077cc",
+          cursor: "pointer",
+          textDecoration: "underline",
+        }}
+        onClick={goHome}
+      >
+        Quay về trang chủ
+      </p>
     </div>
   );
 };
